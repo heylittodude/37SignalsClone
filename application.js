a3af949be506b3b1e530c91e1a56335191768ac9
Howard Xue <howard_xue@hotmail.com>
2013-12-15 23:11:15 -0800

    js file added

---
+++ application.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	$('div').css("background-color", "#ff0000");
+});
+
+/*$(document).ready(function() {
+	$('.clickable').hover(function() {
+		$('#main').replaceWith("<h1>Testing</h1>");
+	});
+});
