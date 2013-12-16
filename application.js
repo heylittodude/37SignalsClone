0a2d97b9ba9d9a06dd4e2aaab2cc1c0bdd4ca349
Howard Xue <howard_xue@hotmail.com>
2013-12-16 01:50:10 -0800

    erased some jQuery code

--- application.js
+++ application.js
@@ -1,9 +1,3 @@
 $(document).ready(function() {
 	$('div').css("background-color", "#ff0000");
 });
-
-/*$(document).ready(function() {
-	$('.clickable').hover(function() {
-		$('#main').replaceWith("<h1>Testing</h1>");
-	});
-});
