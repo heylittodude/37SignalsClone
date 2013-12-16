29191e11806b4d6fa00d60768f8ffa671167712a
Howard Xue <howard_xue@hotmail.com>
2013-12-16 01:45:57 -0800

    JS edited

--- application.js
+++ application.js
@@ -1,9 +1,29 @@
 $(document).ready(function() {
-	$('div').css("background-color", "#ff0000");
-});
-
-/*$(document).ready(function() {
-	$('.clickable').hover(function() {
-		$('#main').replaceWith("<h1>Testing</h1>");
+	$('#swap1').hide();
+	$('#swap2').hide();
+	$('#swap3').hide();
+	$(document).on('mouseenter', '#hover1', function() {
+		$('#swap1').toggle();
+		$('#main').toggle();	
+	});
+	$(document).on('mouseleave', '#hover1', function() {
+		$('#swap1').toggle();
+		$('#main').toggle();
+	});
+	$(document).on('mouseenter', '#hover2', function() {
+		$('#swap2').toggle();
+		$('#main').toggle();	
+	});
+	$(document).on('mouseleave', '#hover2', function() {
+		$('#swap2').toggle();
+		$('#main').toggle();
+	});
+	$(document).on('mouseenter', '#hover3', function() {
+		$('#swap3').toggle();
+		$('#main').toggle();	
+	});
+	$(document).on('mouseleave', '#hover3', function() {
+		$('#swap3').toggle();
+		$('#main').toggle();
 	});
 });
